9159574d158f534a521ff10ee48485953fe5f455
zkthernum <kevin91cooland@gmail.com>
2024-02-13 00:34:10 +0800

    add BufWriter

--- src/kv.rs
+++ src/kv.rs
@@ -3,7 +3,7 @@
 use crate::{KvsError, Result};
 use std::{
     collections::HashMap,
-    io::{BufReader, Read, Seek, SeekFrom},
+    io::{BufReader, BufWriter, Read, Seek, SeekFrom, Write},
     ops::Range,
     path::PathBuf,
 };
@@ -69,6 +69,36 @@ impl<R: Read + Seek> Read for BufReaderWithPos<R> {
 
 impl<R: Read + Seek> Seek for BufReaderWithPos<R> {
     fn seek(&mut self, _pos: std::io::SeekFrom) -> std::io::Result<u64> {
+        todo!()
+    }
+}
+
+struct BufWriterWithPos<W: Write + Seek> {
+    buf: BufWriter<W>,
+    pos: u64,
+}
+
+impl<W: Write + Seek> BufWriterWithPos<W> {
+    fn new(mut inner: W) -> Result<Self> {
+        let pos = inner.seek(SeekFrom::Current(0))?;
+        Ok(Self {
+            buf: BufWriter::new(inner),
+            pos,
+        })
+    }
+}
+
+impl<W: Write + Seek> Write for BufWriterWithPos<W> {
+    fn write(&mut self, _buf: &[u8]) -> std::io::Result<usize> {
+        todo!()
+    }
+    fn flush(&mut self) -> std::io::Result<()> {
+        todo!()
+    }
+}
+
+impl<W: Write + Seek> Seek for BufWriterWithPos<W> {
+    fn seek(&mut self, _pos: SeekFrom) -> std::io::Result<u64> {
         todo!()
     }
 }
