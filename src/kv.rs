e63bce7fe582a4a231056ffec99cd52c12ffce42
zkthernum <kevin91cooland@gmail.com>
2024-02-13 01:08:59 +0800

    finish Buffering

--- src/kv.rs
+++ src/kv.rs
@@ -47,7 +47,7 @@ impl From<(u64, Range<u64>)> for CommandPos {
 }
 
 struct BufReaderWithPos<R: Read + Seek> {
-    buf: BufReader<R>,
+    reader: BufReader<R>,
     pos: u64,
 }
 
@@ -55,26 +55,29 @@ impl<R: Read + Seek> BufReaderWithPos<R> {
     fn new(mut inner: R) -> Result<Self> {
         let pos = inner.seek(SeekFrom::Current(0))?;
         Ok(Self {
-            buf: BufReader::new(inner),
+            reader: BufReader::new(inner),
             pos,
         })
     }
 }
 
 impl<R: Read + Seek> Read for BufReaderWithPos<R> {
-    fn read(&mut self, _buf: &mut [u8]) -> std::io::Result<usize> {
-        todo!()
+    fn read(&mut self, buf: &mut [u8]) -> std::io::Result<usize> {
+        let len = self.reader.read(buf)?;
+        self.pos += len as u64;
+        Ok(len)
     }
 }
 
 impl<R: Read + Seek> Seek for BufReaderWithPos<R> {
-    fn seek(&mut self, _pos: std::io::SeekFrom) -> std::io::Result<u64> {
-        todo!()
+    fn seek(&mut self, pos: std::io::SeekFrom) -> std::io::Result<u64> {
+        self.pos = self.reader.seek(pos)?;
+        Ok(self.pos)
     }
 }
 
 struct BufWriterWithPos<W: Write + Seek> {
-    buf: BufWriter<W>,
+    writer: BufWriter<W>,
     pos: u64,
 }
 
@@ -82,23 +85,26 @@ impl<W: Write + Seek> BufWriterWithPos<W> {
     fn new(mut inner: W) -> Result<Self> {
         let pos = inner.seek(SeekFrom::Current(0))?;
         Ok(Self {
-            buf: BufWriter::new(inner),
+            writer: BufWriter::new(inner),
             pos,
         })
     }
 }
 
 impl<W: Write + Seek> Write for BufWriterWithPos<W> {
-    fn write(&mut self, _buf: &[u8]) -> std::io::Result<usize> {
-        todo!()
+    fn write(&mut self, buf: &[u8]) -> std::io::Result<usize> {
+        let len = self.writer.write(buf)?;
+        self.pos += len as u64;
+        Ok(len)
     }
     fn flush(&mut self) -> std::io::Result<()> {
-        todo!()
+        self.writer.flush()
     }
 }
 
 impl<W: Write + Seek> Seek for BufWriterWithPos<W> {
-    fn seek(&mut self, _pos: SeekFrom) -> std::io::Result<u64> {
-        todo!()
+    fn seek(&mut self, pos: SeekFrom) -> std::io::Result<u64> {
+        self.pos = self.writer.seek(pos)?;
+        Ok(self.pos)
     }
 }
